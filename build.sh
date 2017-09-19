f5085f4d506f7ac758ba3bdbb6599465fa213776
Peter Organisciak <organisciak@gmail.com>
2017-09-18 18:40:42 -0600

    Add index

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 name=$1
 pandoc --slide-level 2 -s -t revealjs -V theme=white -V transition=fade -V slideNumber=true -V history=true --mathjax $name.md -o docs/$name.html
+# Renew TOC
+ls *md | perl -pe "s:^(\d)+-(.*)\.md:- [\2](\1-\2.html):g" | pandoc -f markdown -t html -o index.html
+echo "https://github.com/organisciak/User-Access-Services/blob/master/$name.md http://www.porganized.com/User-Access-Services/$name.html"
